0ad431a5e22a1169a18929b9753277f7ab26215a
Morten Noest <mortennost@gmail.com>
2012-09-13 15:47:14 +0200

    Added XNA mappe2 folder + project

---
+++ XNA Mappe #2/GameStateManagementSample/GameObjects/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input.Touch;
+using System.IO;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+
+
+namespace GameStateManagement.GameObjects
+{
+    public class Player : DrawableGameComponent
+    {
+        SpriteFont gameFont;
+        ContentManager content;
+
+        public Player(Game game) 
+            :base(game)
+        {
+        
+        }
+        
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void LoadContent() 
+        {
+            if (content == null)
+                content = new ContentManager(ScreenManager.Game.Services, "Content");
+
+            gameFont = content.Load<SpriteFont>("gamefont");
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(gameFont, "// TODO", playerPosition, Color.Green);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+	
+    }
+}
